105d540437e77cdb5fc8ed9a8a52ccace0373aaa
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-28 02:54:58 +0300

    Initialised the cat array

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your solution here!
+//Initialised the cat array
+let cats = ["Milo", "Otis", "Garfield"];
+
+//created a function to add an element to the end
+let destructivelyAppendCat = (name)=>{
+    cats.push(name)
+}
